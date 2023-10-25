a074785a1f34bf8afd04930f366764300bd2b99e
Gabriel Cardoso <cardosogabn@gmail.com>
2023-10-25 12:16:08 -0300

    update 25oct

--- gamerating/models.py
+++ gamerating/models.py
@@ -9,6 +9,6 @@ class Game(models.Model):
     genre = models.CharField(max_length=20)
 
 class Rating(models.Model):
-    score = models.SmallIntegerField(max=5, default=3)
-    game = models.ForeignKey(Game)
+    score = models.SmallIntegerField(default=3)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
     
