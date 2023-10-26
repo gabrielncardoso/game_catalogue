56ccc25cc29fb92a08602eda344d106e16c47f9f
Gabriel Cardoso <cardosogabn@gmail.com>
2023-10-26 09:43:48 -0300

    update 25oct
    worked on models.py updates

--- gamerating/models.py
+++ gamerating/models.py
@@ -1,14 +1,26 @@
 from django.db import models
+from django.db.models import Avg
 
 class User(models.Model):
     pass
+    #VIEW LIST OF GAMES AND THEIR COLUMNS
+    #ONLY CAN VIEW THE AVERAGE RATING OF EACH GAME
+    #GIVE A SCORE TO A GAME
 
 class Game(models.Model):
     name = models.CharField(max_length=50)
     nplayers = models.CharField(max_length=5)
     genre = models.CharField(max_length=20)
 
+    def __str__(self):
+        return (f'{self.name}, {self.nplayers}, {self.genre}')
+    
+    def avgscore(self):
+        return self.rating_set.aggregate(Avg("score"))
+
 class Rating(models.Model):
     score = models.SmallIntegerField(default=3)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return (f'{self.score}, {self.game.name}')
